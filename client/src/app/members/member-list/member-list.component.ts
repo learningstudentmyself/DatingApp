6af3b33f0e12e59a4846d4f4e838bf3c90e3d989
Learning Student <learningstudentmyself@gmail.com>
2021-01-06 23:08:43 -0800

    Added member detail - End of section 9

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/model/member';
+import { MembersService } from 'src/app/_services/members.service';
+import { MemberDetailComponent } from '../member-detail/member-detail.component';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-
-  constructor() { }
+  members: Member[];
+  constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe(members => {
+      this.members = members;
+    })
   }
 
 }
